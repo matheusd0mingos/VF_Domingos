6670c8a36cab7a33d3668122d8cdb5abe17fca7d
matheusdomingos007 <matheus@LAPTOP-06R8HR32.localdomain>
2021-07-16 12:26:02 -0300

    Atualização questão 3

--- Questao3/Sistemas.py
+++ Questao3/Sistemas.py
@@ -3,22 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Leitor():
-    def __init__(self, usina):
+    def __init__(self, usina, control):
         self.usina=usina
+        self.control=control
 
     def level_time(self):
         level=np.array([])
         nivel=self.usina.get_sensor()
-        if self.usina.get_actuator():
-            while self.get_actuator():
+        aux=self.usina.get_actuator()
+        i=0
+        while aux:
                 level=np.append(level, nivel)
                 nivel=self.usina.get_sensor()
-                if nivel<=0:
-                    break
-            x=np.arange(0,len(level))*0.1
-        else:
-            level=np.append(level, nivel)
-            x=0
+                aux=self.usina.get_actuator()  
+                self.control.check()         
+        x=np.arange(0, len(level))*0.1
+
+        
         return x, level
 
 class Controlador():
@@ -27,21 +28,21 @@ class Controlador():
 
     def abrir(self):
         self.usina.set_actuator(True)
-        print('Comporta aberta com sucesso')
+        #print('Comporta aberta com sucesso')
     def fechar(self):
-        self.usina.set_actuator(True)
-        print('Comporta fechada com sucesso')
+        self.usina.set_actuator(False)
+        #print('Comporta fechada com sucesso')
 
     def check(self):
         if self.usina.get_sensor()>=245*70/90+10:
             self.abrir()
-        elif self.usina.get_sensor()<=0:
+        else:
             self.fechar()
 
 
 usina1=Usina()
 control1=Controlador(usina1)
-leitor1=Leitor(usina1)
+leitor1=Leitor(usina1, control1)
 control1.check()
 x, level=leitor1.level_time()
 plt.plot(x,level)
